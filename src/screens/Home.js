f65ef9923be61e7fc5b09b15adbf2ffea7bd584e
piyushbansal210 <piyushbansal210@gmail.com>
2023-04-10 05:43:38 +0530

    complete

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,56 +1,66 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+import React from "react";
 
-export default function Home({navigation}) {
-  
+export default function Home({ navigation }) {
+  function pressStart() {
+    navigation.navigate("Main");
+    // console.log("piyush");
+  }
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Legal Text</Text>
-        <Text style={[styles.title,{marginTop:-15}]}>to</Text>
-        <View style={{flexDirection: 'row', marginTop:-15}}>
+        <Text style={[styles.title, { marginTop: -15 }]}>to</Text>
+        <View style={{ flexDirection: "row", marginTop: -15 }}>
           <Text style={styles.titleLaymen}> Laymen</Text>
           <Text style={styles.title}> Text</Text>
         </View>
       </View>
-      <TouchableOpacity style={styles.buttonStarted} onPress={()=>navigation.navigate('Main')}>
+      <TouchableOpacity
+        style={styles.buttonStarted}
+        onPress={() => pressStart()}
+      >
         <Text style={styles.buttonText}>GET STARTED</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1D242E',
-    alignItems:'center',
-    justifyContent:'center'
+    backgroundColor: "#1D242E",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
-    color: 'white',
-    fontFamily: 'CBold',
+    color: "white",
+    fontFamily: "CBold",
     fontSize: 55,
   },
   titleLaymen: {
-    color: '#DAA134',
-    fontFamily: 'CBold',
+    color: "#DAA134",
+    fontFamily: "CBold",
     fontSize: 55,
   },
-  titleContainer:{
-    alignItems:'center'
+  titleContainer: {
+    alignItems: "center",
   },
-  buttonStarted:{
-    backgroundColor:'white',
-    paddingHorizontal:70,
-    paddingVertical:10,
-    marginTop:20,
-    borderRadius:10
+  buttonStarted: {
+    backgroundColor: "white",
+    paddingHorizontal: 70,
+    paddingVertical: 10,
+    marginTop: 20,
+    borderRadius: 10,
   },
-  buttonText:{
-    color:'#DAA134',
-    fontFamily:'PSBold'
-  }
+  buttonText: {
+    color: "#DAA134",
+    fontFamily: "PSBold",
+  },
 });
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,5 +1,5 @@
-import React, {useState, useContext, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
 import {
   View,
   Text,
@@ -7,28 +7,29 @@ import {
   TextInput,
   ScrollView,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
-import LogoHeader from '../components/LogoHeader';
-import NewsContainer from '../components/NewsContainer';
+import LogoHeader from "../components/LogoHeader";
+import NewsContainer from "../components/NewsContainer";
 
-import NEWS from '../assets/data/News';
-import {DataContext} from '../components/DataProvider';
+import NEWS from "../assets/data/News";
+import { DataContext } from "../components/DataProvider";
 
-const Main = ({navigation}) => {
+const Main = ({ navigation }) => {
+  const [inp, setInp] = useState("");
+  const [out, setOut] = useState("");
+  const [news, setNews] = useState([]);
 
-  const [inp, setInp] = useState('');
-  const [out, setOut] = useState('');
-  const [news,setNews]= useState(useContext(DataContext).data.results)
+  const data = useContext(DataContext).data;
 
-  const fetchData = async inpData => {
+  const fetchData = async (inpData) => {
     try {
-      const response = await axios.post('http://10.10.0.67:8000/summarize', {
+      const response = await axios.post("http://10.10.0.67:8080/summarize", {
         text: inpData,
       });
       return response.data;
     } catch (error) {
-      console.log('this is an error');
+      console.log("this is an error");
       console.error(error);
     }
   };
@@ -36,16 +37,18 @@ const Main = ({navigation}) => {
   async function askQues() {
     const input = inp;
     const output = await fetchData(input);
-    console.log(output);
     setOut(output);
   }
 
-  useEffect( () => {
+  useEffect(() => {
     setOut(out);
   }, [out]);
 
-  if(news === null) return null;
-  
+  useEffect(() => {
+    if (data) setNews(data.results);
+  }, [data]);
+
+  if (news.length === 0) return null;
 
   return (
     <ScrollView style={styles.container}>
@@ -54,7 +57,7 @@ const Main = ({navigation}) => {
         <View>
           <TextInput
             value={inp}
-            onChangeText={e => setInp(e)}
+            onChangeText={(e) => setInp(e)}
             style={[
               styles.inputContainer,
               {
@@ -63,13 +66,13 @@ const Main = ({navigation}) => {
             ]}
             multiline={true}
             placeholderTextColor="white"
-            placeholder={'Paste Your Data Here ...'}
+            placeholder={"Paste Your Data Here ..."}
           />
         </View>
         <View>
           <TextInput
             value={out}
-            onChangeText={e => setOut(e)}
+            onChangeText={(e) => setOut(e)}
             style={styles.inputContainer}
             multiline={true}
             placeholderTextColor="white"
@@ -85,7 +88,9 @@ const Main = ({navigation}) => {
       <View style={styles.newsDisplayContainer}>
         <Text style={styles.legalNewsHeader}>LEGAL NEWS</Text>
         {news.map((item, key) => {
-          return <NewsContainer key={key} navigation={navigation} data={item} />;
+          return (
+            <NewsContainer key={key} navigation={navigation} data={item} />
+          );
         })}
       </View>
     </ScrollView>
@@ -95,14 +100,14 @@ const Main = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1D242E',
+    backgroundColor: "#1D242E",
   },
   legalNewsHeader: {
-    color: '#DAA134',
-    fontFamily: 'CBold',
-    alignItems: 'center',
+    color: "#DAA134",
+    fontFamily: "CBold",
+    alignItems: "center",
     fontSize: 25,
-    alignSelf: 'center',
+    alignSelf: "center",
     marginBottom: 10,
     marginTop: 10,
   },
@@ -110,28 +115,28 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   inputContainer: {
-    color: 'white',
-    fontFamily: 'PMedium',
-    backgroundColor: 'rgba(0,0,0,0.3)',
+    color: "white",
+    fontFamily: "PMedium",
+    backgroundColor: "rgba(0,0,0,0.3)",
     marginBottom: 10,
     marginHorizontal: 10,
     height: 200,
-    textAlignVertical: 'top',
+    textAlignVertical: "top",
     padding: 10,
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     marginHorizontal: 10,
     marginTop: 10,
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 20,
     borderRadius: 10,
   },
   buttonText: {
-    color: '#DAA134',
-    fontFamily: 'PSBold',
+    color: "#DAA134",
+    fontFamily: "PSBold",
   },
 });
 
